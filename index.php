a1db8247d4c8e4755daed5a83f7f3d41e6f1eae6
Rizky Ananda F <rizkyanandafaradin@gmail.com>
2023-01-20 22:09:52 +0700

    modify alert, create table task

--- index.php
+++ index.php
@@ -4,6 +4,29 @@ require 'functions.php';
 
 $list = query("SELECT *  FROM task");
 
+
+if (isset($_POST['submit'])) {
+   if (tambah($_POST)>0) {
+      
+   } else {
+      echo "
+      <script> 
+      alert('data gagal ditambahkan');
+      document.location.href = 'tambah.php';
+      </script>
+      ";
+  
+   }
+ }
+
+
+
+
+
+
+
+
+
 ?>
 
 
@@ -18,12 +41,12 @@ $list = query("SELECT *  FROM task");
    <title>Todo List</title>
 </head>
 <body>
-  <main class="container container-sm" " >
+  <main class="container container-sm " " >
    <h1>
       Todo List Application with PHP and MySQL 
    </h1>
 
-   <div class="container text-center">
+   <div class="container text-center scrollbar" >
       <div class="row">
          <div class="col-sm-8">
 
@@ -74,31 +97,56 @@ $list = query("SELECT *  FROM task");
          <!-- End tabel todolist -->
       
       
+               <!-- Start tabel todolist -->
+               <div class="col-sm-4">
+                  <form action="" method="post">
+                     <div class="card border-info mb-3" style="max-width: 18rem;">
+                        <div class="card-header" style="font-weight: bold" >Add Task</div>
+                        <div class="card-body">
+                           <div class="input-group mb-3">
+                              <label class="input-group-text" for="tanggal">Date</label>
+                              <input type="date" class="form-control"  aria-label="date"  name="tanggal" id="tanggal">
+                           </div>
+                           <div class="input-group mb-3">
+                              <label class="input-group-text" for="task">Task</label>
+                              <input type="text" class="form-control" placeholder="Task" aria-label="Username"  name="task" id="task">
+                           </div>
+                            <div id="liveAlertPlaceholder"></div>
+                           
+                           <button type="submit" class="btn btn-outline-info" name="submit" id="liveAlertBtn" >Add</button>
+                        </div>
+                     </div>
+                  </form>
+               </div>
+             
+
+
+
+
+
+
+
+
+
+
+
+
       
-      
-         <!-- Start tabel todolist -->
-      
-      <div class="col-sm-4">
-         <h5>Add Task</h5>
-          <div class="input-group mb-3">
-             <span class="input-group-text" id="basic-addon1">Date</span>
-             <input type="date" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
          </div>
-         <div class="input-group mb-3">
-             <span class="input-group-text" id="basic-addon1">Task</span>
-             <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-         </div>
-         <button type="button" class="btn btn-outline-info">Info</button>
       </div>
-   </div>
-   </div>
+   
 
 
 
+    
 
 
 </main>
 
-<script src="bootstrap-5.3.0-alpha1-dist/js/bootstrap.min.js"></script>
+<h1>dsaasdas</h1>
+
+
+<script src="index.js"></script>
+
 </body>
 </html>
