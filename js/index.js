e09cc02d00a850338c1b4d0e1de1d09920c75032
Rizky Ananda F <rizkyanandafaradin@gmail.com>
2023-01-21 19:47:35 +0700

    add feature search

---
+++ js/index.js
@@ -0,0 +1,54 @@
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
+
+const alert = (message, type) => {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = [`<div class="alert alert-${type} alert-dismissible" role="alert">`, `   <div>${message}</div>`, '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>', '</div>'].join('');
+
+    alertPlaceholder.append(wrapper);
+};
+
+const alertTrigger = document.getElementById('liveAlertBtn');
+if (alertTrigger) {
+    alertTrigger.addEventListener('click', () => {
+        alert('Add Task Successfully', 'success');
+    });
+}
+
+// select input element
+const search = document.getElementById('search');
+
+// add event listener for input
+search.addEventListener('input', function () {
+    // get input value
+    const value = search.value.toLowerCase();
+
+    // select all table rows
+    const rows = document.querySelectorAll('table tr');
+
+    // loop through rows
+    rows.forEach(function (row) {
+        // select all cells in row
+        const cells = row.querySelectorAll('td');
+
+        // check if cells contain input value
+        let match = false;
+        cells.forEach(function (cell, index) {
+            if (index === 0 || index === 1) {
+                if (cell.textContent.toLowerCase().indexOf(value) > -1) {
+                    match = true;
+                }
+            } else if (index === 2) {
+                if (cell.textContent.toLowerCase().indexOf(value) > -1) {
+                    match = true;
+                }
+            }
+        });
+
+        // show or hide row based on match
+        if (match) {
+            row.style.display = '';
+        } else {
+            row.style.display = 'none';
+        }
+    });
+});
